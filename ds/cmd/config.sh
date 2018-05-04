1c5858d688beb8e0f76a6f54de66357972afd7c8
dashohoxha <dashohoxha@gmail.com>
2018-05-04 19:40:32 +0200

    Fix script redis-config.sh

--- ds/cmd/config.sh
+++ ds/cmd/config.sh
@@ -12,6 +12,7 @@ cmd_config() {
     ds inject install/drupal-make.sh
     ds inject install/drupal-install.sh
     ds inject install/drupal-config.sh
+    ds inject install/redis-config.sh
     ds inject install/drush-config.sh
     ds inject install/apache2.sh
     ds inject install/set-prompt.sh
@@ -29,6 +30,4 @@ cmd_config() {
 
     # drush may create some files with wrong permissions, fix them
     ds inject fix-file-permissions.sh
-
-    ds inject install/redis-config.sh
 }
--- ds/scripts/install/redis-config.sh
+++ ds/scripts/install/redis-config.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -x
+
 source /host/settings.sh
-### check if REDIS_HOST is not empty, else exit
-[[ -n $REDIS_HOST ]] || exit 0 
 
-drupal_settings=$DRUPAL_DIR/sites/default/settings.php
-drush="drush --root=$DRUPAL_DIR --yes"
-$drush dl redis # use first site e.g. @local_proj
-apt install -y php-redis redis-tools
+[[ -n $REDIS_HOST ]] || exit 0
 
+apt -y install php-redis
+drush --root=$DRUPAL_DIR --yes dl redis
 
-cat << EOF "
+cat << EOF >> $DRUPAL_DIR/sites/default/settings.php
 // Redis settings
 \$conf['redis_client_interface'] = 'PhpRedis';
 \$conf['redis_client_host'] = '$REDIS_HOST';
@@ -17,4 +15,5 @@ cat << EOF "
 \$conf['path_inc'] = 'sites/all/modules/redis/redis.path.inc';
 \$conf['cache_backends'][] = 'sites/all/modules/redis/redis.autoload.inc';
 \$conf['cache_default_class'] = 'Redis_Cache';
-" >> $drupal_settings
+\$conf['cache_prefix'] = '$CONTAINER';
+EOF
